560673879c41cb8c55c19ca12d7fe47e8791ecea
NBESS <nicholasdbess@gmail.com>
2020-05-16 23:51:21 -0400

    adds new html layout and styles

--- app.js
+++ app.js
@@ -79,12 +79,31 @@ app.put('/api/todos/:id', (req, res) =>{
     id: Number.parseInt(req.params.id),
     todo: req.body.todo,
   }
-  let todoIndex = todoList.findIndex((todo)=>todo.id == req.params.id)
+  let todoIndex = todoList.findIndex((todo)=>todo.id == req.params.id);
   todoList[todoIndex] = updatedTodo;
 })
 
 // DELETE /api/todos/:id
+app.delete('/api/todos/:id', (req, res) => {
+  // identify todo index (:id) to remove from todoList using findIndex
+  let indexToDelete = todoList.findIndex((todo)=>todo.id == req.params.id);
+  
+  // remove todo (":id" i.e. req.params.id) from array using the splice method
+  let removedTodo = todoList.splice(indexToDelete, 1)
+  res.json(removedTodo);
+})
+
 
 app.listen(port, () => {
   console.log('Todo List API is now listening on port ' + "http://localhost:" + port);
 })
+
+
+// Example removing an item from an array using the splice method 
+// const myArray = ['cats', 'dogs', 'rabbits', 'reptiles'];
+
+// console.log(myArray);
+// let popped = myArray.splice(2, 1);
+// console.log(popped);
+// console.log(myArray);
+
